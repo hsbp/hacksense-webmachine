d5cc0235af05a2fc9978945d3bc4042d84533b79
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-18 14:20:13 +0200

    new_webmachine.sh + .gitignore

---
+++ src/hacksense_app.erl
@@ -0,0 +1,21 @@
+%% @author author <author@example.com>
+%% @copyright YYYY author.
+
+%% @doc Callbacks for the hacksense application.
+
+-module(hacksense_app).
+-author('author <author@example.com>').
+
+-behaviour(application).
+-export([start/2,stop/1]).
+
+
+%% @spec start(_Type, _StartArgs) -> ServerRet
+%% @doc application start callback for hacksense.
+start(_Type, _StartArgs) ->
+    hacksense_sup:start_link().
+
+%% @spec stop(_State) -> ServerRet
+%% @doc application stop callback for hacksense.
+stop(_State) ->
+    ok.
