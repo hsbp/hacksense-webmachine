924efae26572d6ce66b97b2e16a46e2b6579151a
András Veres-Szentkirályi <vsza@vsza.hu>
2013-10-20 16:18:40 +0200

    restructured resource for cleaner design

--- src/hacksense_resource.erl
+++ src/hacksense_resource.erl
@@ -1,63 +1,76 @@
 -module(hacksense_resource).
--export([init/1, to_html/2, generate_etag/2]).
+-export([init/1, generate_etag/2, content_types_provided/2]).
+-export([to_html/2, to_csv/2, to_rss/2, to_xml/2, to_txt/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 -include_lib("stdlib/include/qlc.hrl").
 -include_lib("hacksense_status.hrl").
 
 -define(ISO_DATETIME_FMT, "~4.10.0B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B").
+-define(CT_CSV, {"text/csv", to_csv}).
+-define(CT_RSS, {"application/rss+xml", to_rss}).
+-define(CT_XML, {"text/xml", to_xml}).
+-define(CT_TXT, {"text/plain", to_txt}).
+-define(CT_HTML, {"text/html", to_html}).
 
-init([]) -> {ok, undefined}.
+%% Webmachine Resource functions
 
-generate_etag(ReqData, State) ->
-    {(get_status())#hacksense_status.id, ReqData, State}.
+init([Model, Format]) -> {ok, {Model, Format, fetch_model_data(Model)}}.
 
-to_html(ReqData, State) ->
-    Base = wrq:path_info(base, ReqData),
-    Body = case Base of
-        "submit" ->
-            handle_submit(wrq:disp_path(ReqData)), "OK";
-        "history.csv" -> csv_history(get_history());
-        "history.xml" -> xml_history(get_history());
-        "history" -> html_history(get_history());
-        "status.csv" -> format_csv(get_status());
-        "status.txt" -> format_human(txt, get_status());
-        "status.xml" -> format_xml(get_status());
-        "status.rss" -> format_rss(get_status());
-        "status" -> format_human(html, get_status());
-        undefined -> home_page()
-    end,
-    ContentType = content_type(Base),
-    {Body, wrq:set_resp_header("Content-Type", ContentType, ReqData), State}.
+generate_etag(ReqData, {_, history, History} = State) ->
+    Digest = base64:encode_to_string(erlsha2:sha256(term_to_binary(History))),
+    {Digest, ReqData, State};
+generate_etag(ReqData, {_, status, Status} = State) ->
+    {Status#hacksense_status.id, ReqData, State};
+generate_etag(ReqData, State) -> {undefined, ReqData, State}.
 
-content_type(undefined) -> "text/html; charset=UTF-8";
-content_type("submit") -> "text/plain";
-content_type(Name) ->
-    case filename:extension(Name) of
-        "csv" -> "text/csv";
-        "xml" -> "text/xml";
-        "rss" -> "application/rss+xml";
-        "txt" -> "text/plain";
-        _ -> "text/html"
-    end ++ "; charset=UTF-8".
+content_types_provided(ReqData, {_, csv, _} = State) -> {[?CT_CSV], ReqData, State};
+content_types_provided(ReqData, {_, rss, _} = State) -> {[?CT_RSS], ReqData, State};
+content_types_provided(ReqData, {_, xml, _} = State) -> {[?CT_XML], ReqData, State};
+content_types_provided(ReqData, {_, txt, _} = State) -> {[?CT_TXT], ReqData, State};
+content_types_provided(ReqData, State) ->
+    {[?CT_HTML, ?CT_CSV, ?CT_RSS, ?CT_XML, ?CT_TXT], ReqData, State}.
 
-home_page() -> {ok, Content} = home_dtl:render([]), Content.
+%% HTML
 
-html_history(History) ->
-    Events = [{S#hacksense_status.id, timestamp_to_isofmt(S),
-               status_to_open_closed(S)} || S <- History],
-    {ok, Content} = history_html_dtl:render([{events, Events}]),
-    Content.
+to_html(ReqData, {Model, _, Data} = State) ->
+    Module = list_to_atom(atom_to_list(Model) ++ "_dtl"),
+    {ok, Content} = Module:render(dtl_params(Model, Data)),
+    {Content, ReqData, State}.
 
-csv_history(History) ->
-    lists:map(fun format_csv/1, History).
+dtl_params(home, _) -> [];
+dtl_params(history, History) ->
+    [{events, [{S#hacksense_status.id, timestamp_to_isofmt(S),
+               status_to_open_closed(S)} || S <- History]}];
+dtl_params(status, Status) ->
+    {OpenClosed, Since} = human_status(Status),
+    [{open_closed, OpenClosed}, {since, Since}].
 
-format_human(Format, Status) ->
-    OpenClosed = status_to_open_closed(Status),
+%% CSV
+
+to_csv(ReqData, {status, _, Status} = State) ->
+    {format_csv(Status), ReqData, State};
+to_csv(ReqData, {history, _, History} = State) ->
+    {lists:map(fun format_csv/1, History), ReqData, State}.
+
+format_csv(Status) ->
     Since = timestamp_to_isofmt(Status),
-    format_human(Format, OpenClosed, Since).
+    io_lib:format("~s;~s;~B\n",
+        [Status#hacksense_status.id, Since, Status#hacksense_status.status]).
 
-format_rss(Status) ->
+%% TXT
+
+to_txt(ReqData, {status, _, Status} = State) ->
+    {OpenClosed, Since} = human_status(Status),
+    Content = ["H.A.C.K. is currently ", OpenClosed, " since ", Since, "\n"],
+    {Content, ReqData, State};
+to_txt(ReqData, {submit, _, _} = State) ->
+    handle_submit(wrq:path_info(data, ReqData)),
+    {"OK\n", ReqData, State}.
+
+%% RSS
+
+to_rss(ReqData, {status, _, Status} = State) ->
     ItemContents = [{title, ["H.A.C.K. has ", status_to_open_closed(Status, "opened", "closed")]},
                    {guid, ["http://vsza.hu/hacksense/state_changes/", Status#hacksense_status.id]},
                    {pubDate, [webmachine_util:rfc1123_date(Status#hacksense_status.timestamp)]}],
@@ -65,14 +78,15 @@ format_rss(Status) ->
                          {link, ["http://vsza.hu/hacksense/"]},
                           description,
                          {item, ItemContents}]},
-    xmerl:export_simple([{rss, [{version, "2.0"}], [Channel]}], xmerl_xml).
+    RSS = xmerl:export_simple([{rss, [{version, "2.0"}], [Channel]}], xmerl_xml),
+    {RSS, ReqData, State}.
 
-format_human(html, OpenClosed, Since) ->
-    {ok, Content} = status_html_dtl:render(
-                      [{open_closed, OpenClosed}, {since, Since}]),
-    Content;
-format_human(txt, OpenClosed, Since) ->
-    ["H.A.C.K. is currently ", OpenClosed, " since ", Since, "\n"].
+%% XML
+
+to_xml(ReqData, {status, _, Status} = State) ->
+    {format_xml(Status), ReqData, State};
+to_xml(ReqData, {history, _, History} = State) ->
+    {xml_history(History), ReqData, State}.
 
 xml_history(History) ->
     Children = lists:map(fun status_xml/1, History),
@@ -86,10 +100,12 @@ status_xml(S) ->
     {state_change, [{id, S#hacksense_status.id}, {'when', Since},
                     {what, S#hacksense_status.status}], []}.
 
-format_csv(Status) ->
+%% Common conversion functions
+
+human_status(Status) ->
+    OpenClosed = status_to_open_closed(Status),
     Since = timestamp_to_isofmt(Status),
-    io_lib:format("~s;~s;~B\n",
-        [Status#hacksense_status.id, Since, Status#hacksense_status.status]).
+    {OpenClosed, Since}.
 
 status_to_open_closed(Status) ->
     status_to_open_closed(Status, "open", "closed").
@@ -102,14 +118,17 @@ status_to_open_closed(Status, Open, Closed) ->
 timestamp_to_isofmt(#hacksense_status{timestamp={{Y, Mo, D}, {H, Mn, S}}}) ->
     io_lib:format(?ISO_DATETIME_FMT, [Y, Mo, D, H, Mn, S]).
 
-get_status() ->
+%% Data access functions
+
+fetch_model_data(status) ->
     case get_date_ordered_statuses(descending, 1) of
          [Top1] -> Top1;
          [] -> undefined;
          Error -> Error
-    end.
-
-get_history() -> get_date_ordered_statuses(ascending, all_remaining).
+    end;
+fetch_model_data(history) ->
+    get_date_ordered_statuses(ascending, all_remaining);
+fetch_model_data(_) -> undefined.
 
 get_date_ordered_statuses(OrderDir, Number) ->
     {atomic, Rows} = mnesia:transaction(fun() ->
